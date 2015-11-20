4bbb4267f9463a7bd704589cac78f48f02d56b8b
Dylan Scott <dylanrhysscott@gmail.com>
2015-11-20 15:26:35 +0000

    Remove logging

--- app/routes.js
+++ app/routes.js
@@ -3,9 +3,7 @@
     var opts = {
       root: __dirname + '/views/www/'
     };
-    console.log('Routes');
     app.get('/', function(req, res) {
-      console.log('Works');
       res.render('index');
     });
 })();
