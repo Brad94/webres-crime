d3367b5a8240ad9b8820b7b1c585f1e82f0aafe8
Dylan Scott <dylanrhysscott@gmail.com>
2015-11-20 15:41:02 +0000

    JSON client

--- app/routes.js
+++ app/routes.js
@@ -4,6 +4,16 @@
       root: __dirname + '/views/www/'
     };
     app.get('/', function(req, res) {
-      res.render('index');
+
+      fs.readFile(__dirname + "/views/data/data.json", 'utf8', function(err, data) {
+        if(err) {
+          console.log(err)
+        } else {
+          var json = JSON.parse(data);
+          res.render('index', json);
+        }
+      });
+
+
     });
 })();
