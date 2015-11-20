e40b9aad9ca0b07e037dcd1177d86ce77820c255
Dylan Scott <dylanrhysscott@gmail.com>
2015-11-20 15:11:56 +0000

    Added missing dep

--- app/routes.js
+++ app/routes.js
@@ -2,8 +2,10 @@
     var fs = require('fs');
     var opts = {
       root: __dirname + '/views/www/'
-    }
+    };
+    console.log('Routes');
     app.get('/', function(req, res) {
+      console.log('Works');
       res.render('index');
     });
 })();
