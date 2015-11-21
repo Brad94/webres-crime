66298e8b79cd703be5c35a560d21b5de94803a9c
Dylan Scott <dylanrhysscott@gmail.com>
2015-11-21 20:07:42 +0000

    Change default zoom

--- app/views/static/script.js
+++ app/views/static/script.js
@@ -5,7 +5,7 @@
     },
 
     loadMap: function() {
-      var map = L.map('map').setView([51.505, -0.09], 13);
+      var map = L.map('map').setView([51.505, -0.09], 5);
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
         maxZoom: 30,
