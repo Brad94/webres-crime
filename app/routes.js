44a241d154a965112be3e4b9fa0c394d51c07000
Dylan Scott <dylanrhysscott@gmail.com>
2015-11-25 21:42:57 +0000

    Read Directory

--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,5 @@
 (function() {
     var fs = require('fs');
-    var opts = {
-      root: __dirname + '/views/www/'
-    };
     app.get('/', function(req, res) {
 
       fs.readFile(__dirname + "/views/data/data.json", 'utf8', function(err, data) {
@@ -34,6 +31,24 @@
         }
       });
 
+    });
+
+    app.get('/models', function(req, res) {
+      fs.readdir(__dirname + '/views/data', function(err, data) {
+        if(err) {
+          res.status(500).json({
+            'message': 'Could not read directory',
+            'err': err
+          });
+        }
+
+        if(data) {
+          var json = {
+            files: data
+          };
+          res.status(200).json(json);
+        }
+      })
     });
 
     app.get('/build', function(req, res) {
